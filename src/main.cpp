1d292a213efc4ca5e48e05a6dc84c9064f0b6e94
Richard <rgkirch@gmail.com>
2017-02-13 19:27:06 -0500

    why is sizeof(matrix) 12?

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,20 @@
-#include <Arduino.h>
-#undef min
-#undef max
-#undef abs
-#include <vector>
+#include "Arduino.h"
 
 char inputs[] {14, 15, 16, 17};
 char outputs[] {7,  8,  9, 10, 11, 12, 18, 19, 20, 21, 22, 23};
-char matrix[12*4]; // state of keys last scanned (pressed on not pressed)
+char matrix[48]; // state of keys last scanned (pressed on not pressed) // 12 * 4
 // in 14, 15, 16, 17
 // out  7,  8,  9, 10, 11, 12, 18, 19, 20, 21, 22
 
 void setup()
 {
-    memset(matrix, 0, sizeof(matrix));
+
+    memset(matrix, 0, 12 * 4 * sizeof(char));
     Serial.begin(9600);
     delay(1000);
     Serial.print("test print");
-    Keyboard.begin();
-    Mouse.begin();
+//    Keyboard.begin();
+//    Mouse.begin();
 
     for(char i = 0; i < sizeof(inputs) / sizeof(char); i++)
     {
@@ -33,6 +30,17 @@ void setup()
 }
 void loop()
 {
+    Serial.print("inputs ");
+    Serial.println((int)sizeof(inputs));
+    Serial.print("outputs ");
+    Serial.println((int)sizeof(outputs));
+    Serial.print("matrix ");
+    Serial.println((int)sizeof(outputs));
+//    Mouse.move(4, 0);
+//    delay(25);
+//    Mouse.move(-4, 0);
+//    delay(25);
+//    Serial.println("loop");
     for(char i = 0; i < sizeof(outputs) / sizeof(char); i++)
     {
         digitalWrite(outputs[i], HIGH);
@@ -52,4 +60,5 @@ void loop()
         }
         digitalWrite(outputs[i], LOW);
     }
+//    delay(1000);
 }

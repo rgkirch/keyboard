c6fe36522db1190787904747bc2cd4e35ee0a5a4
Richard <rgkirch@gmail.com>
2017-02-16 19:50:23 -0500

    the order of th ecolums is right now

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include "Arduino.h"
 
-char inputs[] {14, 15, 16, 17};
-char outputs[] {7,  8,  9, 10, 11, 12, 18, 19, 20, 21, 22, 23};
+char inputs[] = {14, 15, 16, 17};
+//char outputs[] {7,  8,  9, 10, 11, 12, 18, 19, 20, 21, 22, 23};
+//                11  6   7   8   9  10   0   1   2   3   4   5
+//char outputs[] = {19, 20, 21, 22, 23, 18, 8, 9, 10, 11, 12, 7};
+//char outputs[] = {23, 19, 21, 22, 18, 20, 8, 9, 10, 11, 12, 7};
+char outputs[] = {23, 18, 19, 20, 21, 22, 7, 8, 9, 10, 11, 12};
+int keymap[] = {'\'', ',', '.', 'p', 'y', 'y', 'f', 'g', 'c', 'r', 'l', '/', '=',
+'key_esc', 'a', 'o', 'e', 'u', 'i', 'd', 'h', 't', 'n', 's', '-',
+'key_left_shift', ';', 'q', 'j', 'k', 'x', 'b', 'm', 'w', 'v', 'z', 'key_right_shift',
+'key_left_ctrl', 'key_left_gui', 'key_left_alt', 'key_left_arrow', 'key_down_arrow', ' ', ' ', ' ', 'key_up_arrow', 'key_right_arrow', 'key_right_alt', 'key_right_ctrl'};
 bool matrix[48] = {false}; // state of keys last scanned (pressed on not pressed) // 12 * 4
 // in 14, 15, 16, 17
 // out  7,  8,  9, 10, 11, 12, 18, 19, 20, 21, 22
@@ -13,7 +21,7 @@ void setup()
     Serial.begin(9600);
     delay(1000);
     Serial.print("test print");
-//    Keyboard.begin();
+    Keyboard.begin();
 //    Mouse.begin();
 
     for(char i = 0; i < sizeof(inputs) / sizeof(char); i++)
@@ -30,23 +38,17 @@ void setup()
 }
 void loop()
 {
-//    Serial.print("inputs ");
-//    Serial.println((int)sizeof(inputs));
-//    Serial.print("outputs ");
-//    Serial.println((int)sizeof(outputs));
-//    Serial.print("matrix ");
-//    Serial.println((int)sizeof(matrix));
 //    Mouse.move(4, 0);
 //    delay(25);
 //    Mouse.move(-4, 0);
 //    delay(25);
 //    Serial.println("loop");
-    delay(200);
-    for (auto c : matrix) {
-        Serial.print(c);
-        Serial.print(" ");
-    }
-    Serial.println();
+//    delay(200);
+//    for (auto c : matrix) {
+//        Serial.print(c);
+//        Serial.print(" ");
+//    }
+//    Serial.println();
     for(int i = 0; i < sizeof(outputs) / sizeof(char); i++)
     {
         digitalWrite(outputs[i], HIGH);
@@ -61,8 +63,10 @@ void loop()
                 if(state)
                 {
                     matrix[location] = 1;
+//                    Keyboard.press(keymap[location]);
                 } else {
                     matrix[location] = 0;
+//                    Keyboard.release(keymap[location]);
                 }
                 Serial.print(i);
                 Serial.print(" ");

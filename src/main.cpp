6c39b257177e357788ba288647d338f457ccac39
Richard <rgkirch@gmail.com>
2017-02-16 21:13:53 -0500

    added an array to track length pressed

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 #include "Arduino.h"
 
 char inputs[] = {14, 15, 16, 17};
-//char outputs[] {7,  8,  9, 10, 11, 12, 18, 19, 20, 21, 22, 23};
-//                11  6   7   8   9  10   0   1   2   3   4   5
-//char outputs[] = {19, 20, 21, 22, 23, 18, 8, 9, 10, 11, 12, 7};
-//char outputs[] = {23, 19, 21, 22, 18, 20, 8, 9, 10, 11, 12, 7};
 char outputs[] = {23, 18, 19, 20, 21, 22, 7, 8, 9, 10, 11, 12};
-int keymap[] = {'\'', ',', '.', 'p', 'y', 'y', 'f', 'g', 'c', 'r', 'l', '/', '=',
+int keymap[] = {'\'', ',', '.', 'p', 'y', 'y', 'f', 'g', 'c', 'r', 'l', '/',
 'key_esc', 'a', 'o', 'e', 'u', 'i', 'd', 'h', 't', 'n', 's', '-',
 'key_left_shift', ';', 'q', 'j', 'k', 'x', 'b', 'm', 'w', 'v', 'z', 'key_right_shift',
 'key_left_ctrl', 'key_left_gui', 'key_left_alt', 'key_left_arrow', 'key_down_arrow', ' ', ' ', ' ', 'key_up_arrow', 'key_right_arrow', 'key_right_alt', 'key_right_ctrl'};
 bool matrix[48] = {false}; // state of keys last scanned (pressed on not pressed) // 12 * 4
+int times[48] = {0}; // state of keys last scanned (pressed on not pressed) // 12 * 4
 // in 14, 15, 16, 17
 // out  7,  8,  9, 10, 11, 12, 18, 19, 20, 21, 22
 
@@ -62,15 +59,18 @@ void loop()
             {
                 if(state)
                 {
-                    matrix[location] = 1;
-//                    Keyboard.press(keymap[location]);
+                    if(millis() - times[location] > 30) {
+                        matrix[location] = 1;
+                        Keyboard.press(keymap[location]);
+                    }
                 } else {
+                    times[location] = millis();
                     matrix[location] = 0;
-//                    Keyboard.release(keymap[location]);
+                    Keyboard.release(keymap[location]);
                 }
-                Serial.print(i);
-                Serial.print(" ");
-                Serial.println(j);
+//                Serial.print(i);
+//                Serial.print(" ");
+//                Serial.println(j);
             }
         }
         digitalWrite(outputs[i], LOW);

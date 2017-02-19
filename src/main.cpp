286c1981cb5e81adbee8a14f357f6daf536a11a4
Richard <rgkirch@gmail.com>
2017-02-19 17:19:22 -0500

    added more keymap arrays

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,37 @@
 
 char inputs[] = {6,  7,  8,  9};
 char outputs[] = {10, 11, 12, 15, 16, 17, 18, 19, 20, 21, 22, 23};
-int keymap[] = {
-        KEY_TAB,	    KEY_QUOTE,     KEY_COMMA,             KEY_PERIOD,	KEY_P,         KEY_Y,     KEY_F,     KEY_G,     KEY_C,     KEY_R,           KEY_L,         KEY_SLASH,
-        KEY_ESC,	    KEY_A,         KEY_O,                 KEY_E,	    KEY_U,         KEY_I,     KEY_D,     KEY_H,     KEY_T,     KEY_N,           KEY_S,         KEY_MINUS,
-        KEY_LEFT_SHIFT, KEY_SEMICOLON, KEY_Q,	              KEY_J,        KEY_K,         KEY_X,     KEY_B,     KEY_M,     KEY_W,     KEY_V,           KEY_Z,         KEY_RIGHT_SHIFT,
-        KEY_ESC,        KEY_LEFT_GUI,  KEY_LEFT_GUI,  MODIFIERKEY_LEFT_CTRL, KEY_LEFT_ALT, KEY_BACKSPACE, KEY_ENTER, KEY_SPACE, KEY_SPACE, KEY_RIGHT_ARROW, KEY_RIGHT_ALT, KEY_RIGHT_CTRL
+int modifiers[] = {
+        KEY_TAB, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        KEY_ESC, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        KEY_LEFT_SHIFT, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, KEY_RIGHT_SHIFT,
+        KEY_ESC, KEY_LEFT_GUI,  KEY_LEFT_GUI,  MODIFIERKEY_LEFT_CTRL, KEY_LEFT_ALT, KEY_BACKSPACE, KEY_ENTER, KEY_SPACE, KEY_SPACE, KEY_RIGHT_ARROW, KEY_RIGHT_ALT, KEY_RIGHT_CTRL
+};
+int qwerty[] = {
+        -1, KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T, KEY_Y, KEY_U, KEY_I, KEY_O, KEY_P, KEY_LEFT_BRACE
+        -1, KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_K, KEY_L, KEY_SEMICOLON, KEY_QUOTE,
+        -1, KEY_Z, KEY_X, KEY_C, KEY_V, KEY_B, KEY_N, KEY_M, KEY_COMMA, KEY_PERIOD, KEY_SLASH, -1
+};
+int dvorak[] = {
+        -1, KEY_QUOTE,     KEY_COMMA, KEY_PERIOD, KEY_P, KEY_Y, KEY_F, KEY_G, KEY_C, KEY_R, KEY_L, KEY_SLASH,
+        -1, KEY_A,         KEY_O,     KEY_E,	  KEY_U, KEY_I, KEY_D, KEY_H, KEY_T, KEY_N, KEY_S, KEY_MINUS,
+        -1, KEY_SEMICOLON, KEY_Q,	  KEY_J,      KEY_K, KEY_X, KEY_B, KEY_M, KEY_W, KEY_V, KEY_Z, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+};
+int function[] = {
+        KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
 };
 bool matrix[48] = {false}; // state of keys last scanned (pressed on not pressed) // 12 * 4
 int times[48] = {0}; // state of keys last scanned (pressed on not pressed) // 12 * 4
-// in 14, 15, 16, 17
-// out  7,  8,  9, 10, 11, 12, 18, 19, 20, 21, 22
+int getKey()
+{
+}
 void setup()
 {
-
+    keymap = base;
 //    memset(matrix, 0, 48 * sizeof(char));
     Serial.begin(9600);
     delay(1000);

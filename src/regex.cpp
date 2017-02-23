8f6c2f8f65a1352f74f2566c3c0cc6b69229c61d
Richie <rgkirch@gmail.com>
2017-02-23 11:00:32 -0500

    should make it work with bytes...

--- src/regex.cpp
+++ src/regex.cpp
@@ -7,10 +7,10 @@ enum {KEY_ENTER, KEY_CTRL, KEY_ALT, KEY_DELETE, KEY_BACKSPACE, KEY_SPACE};
 std::string keyStrings[] {"KEY_ENTER", "KEY_CTRL", "KEY_ALT", "KEY_DELETE", "KEY_BACKSPACE", "KEY_SPACE"};
 
 void press(int key) {
-    std::cout << "press" << keyStrings[key] << std::endl;
+    std::cout << "press " << keyStrings[key] << std::endl;
 }
 void release(int key) {
-    std::cout << "release" << keyStrings[key] << std::endl;
+    std::cout << "release " << keyStrings[key] << std::endl;
 }
 void send(int key) {
     press(key);
@@ -32,67 +32,72 @@ Stack<int> stack;
 
 int main() {
     std::string inputString;
-    while(std:: cin >> inputString) {
+    while(std::getline(std::cin, inputString)) {
         // space and backspace
-        if (std::regex_match(inputString, std::regex("42p 42r"))) {
-            send(KEY_SPACE); stack.pop(2); assert(stack.empty());
-        }
-        if (std::regex_match(inputString, std::regex("41p 41r"))) {
-            send(KEY_BACKSPACE); stack.pop(2); assert(stack.empty());
-        }
-        // enter and delete
-        if (std::regex_match(inputString, std::regex("41p 42p 42r"))) {
-            send(KEY_ENTER);
-        }
-        if (std::regex_match(inputString, std::regex("41p (42p 42r){2}"))) {
-            send(KEY_ENTER); stack.pop(2);
-        }
-        if (std::regex_match(inputString, std::regex("41p 42p 42r 41r"))) {
-            stack.pop(4); assert(stack.empty());
-        }
-        if (std::regex_match(inputString, std::regex("42p 41p 41r"))) {
-            send(KEY_DELETE);
-        }
-        if (std::regex_match(inputString, std::regex("42p (41p 41r){2}"))) {
-            send(KEY_DELETE); stack.pop(2);
-        }
-        if (std::regex_match(inputString, std::regex("42p 41p 41r 42r"))) {
-            stack.pop(4); assert(stack.empty());
-        }
-        // ctrl alt other_key
-        if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp"))) {
-            press(KEY_CTRL); press(KEY_ALT); press(stack.back());
-        }
-        if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) (KEYp) (?:41r|42r)* \1r"))) {
-            release(stack.back());
-        }
-        if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) (KEYp) (?:41r|42r)* \1r (?:41r|42r)* KEYp"))) {
-            press(stack.back()); int t = stack.back(); stack.pop(3); stack.push_back(t);
-        }
-        if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) (?:KEYp (?:41r|42r)*)+ KEYp"))) {
-            press(stack.back());
-        }
-        if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) (?:?=(?:KEYp (?:41r|42r)*)+ KEYp) (?:KEYp* (KEYp) KEYp*) \1r"))) {
-            release(stack.back()); stack.remove(p(stack.back()));
-        }
-        // ctrl alt done
-        if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp+ 41r"))) {
-            release(KEY_CTRL);
-        }
-        if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp+ 42r"))) {
-            release(KEY_ALT);
-        }
-        if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp+ 41r 42r"))) {
-            release(KEY_CTRL); release(KEY_ALT); stack.pop(2); stack.pop_front(2);
-        }
-        if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp+ 42r 41r"))) {
-            release(KEY_ALT); release(KEY_CTRL); stack.pop(2); stack.pop_front(2);
-        }
-        if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp+ 42r 42p"))) {
-            press(KEY_ALT); stack.pop(2);
-        }
-        if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp+ 41r 41p"))) {
-            press(KEY_CTRL); stack.pop(2);
+        try {
+            if (std::regex_match(inputString, std::regex("42p 42r"))) {
+                send(KEY_SPACE);// stack.pop(2); assert(stack.empty());
+            }
+            if (std::regex_match(inputString, std::regex("41p 41r"))) {
+                send(KEY_BACKSPACE);// stack.pop(2); assert(stack.empty());
+            }
+            // enter and delete
+            if (std::regex_match(inputString, std::regex("41p 42p 42r"))) {
+                send(KEY_ENTER);//
+            }
+            if (std::regex_match(inputString, std::regex("41p (42p 42r){2}"))) {
+                send(KEY_ENTER);// stack.pop(2);
+            }
+            if (std::regex_match(inputString, std::regex("41p 42p 42r 41r"))) {
+                //stack.pop(4); assert(stack.empty());
+            }
+            if (std::regex_match(inputString, std::regex("42p 41p 41r"))) {
+                send(KEY_DELETE);//
+            }
+            if (std::regex_match(inputString, std::regex("42p (41p 41r){2}"))) {
+                send(KEY_DELETE);// stack.pop(2);
+            }
+            if (std::regex_match(inputString, std::regex("42p 41p 41r 42r"))) {
+                //stack.pop(4); assert(stack.empty());
+            }
+            // ctrl alt other_key
+            if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp"))) {
+                press(KEY_CTRL); press(KEY_ALT);// press(stack.back());
+            }
+            if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) (KEYp) (?:41r|42r)* \1r"))) {
+                //release(stack.back());
+            }
+            if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) (KEYp) (?:41r|42r)* \1r (?:41r|42r)* KEYp"))) {
+                //press(stack.back());// int t = stack.back(); stack.pop(3); stack.push_back(t);
+            }
+            if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) (?:KEYp (?:41r|42r)*)+ KEYp"))) {
+                //press(stack.back());
+            }
+            if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) (?:?=(?:KEYp (?:41r|42r)*)+ KEYp) (?:KEYp* (KEYp) KEYp*) \1r"))) {
+                //release(stack.back());// stack.remove(p(stack.back()));
+            }
+            // ctrl alt done
+            if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp+ 41r"))) {
+                release(KEY_CTRL);
+            }
+            if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp+ 42r"))) {
+                release(KEY_ALT);
+            }
+            if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp+ 41r 42r"))) {
+                release(KEY_CTRL); release(KEY_ALT);// stack.pop(2); stack.pop_front(2);
+            }
+            if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp+ 42r 41r"))) {
+                release(KEY_ALT); release(KEY_CTRL);// stack.pop(2); stack.pop_front(2);
+            }
+            if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp+ 42r 42p"))) {
+                press(KEY_ALT);// stack.pop(2);
+            }
+            if (std::regex_match(inputString, std::regex("(?:41p 42p | 42p 41p) KEYp+ 41r 41p"))) {
+                press(KEY_CTRL);// stack.pop(2);
+            }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
         }
     }
+    return 0;
 }

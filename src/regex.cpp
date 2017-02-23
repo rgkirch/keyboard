9b88b5cb176e7e568af42c5694284259614214be
Richie <rgkirch@gmail.com>
2017-02-23 10:09:09 -0500

    gonig to try to match on input

--- src/regex.cpp
+++ src/regex.cpp
@@ -31,46 +31,49 @@ private:
 Stack<int> stack;
 
 int main() {
-    // space and backspace
-    std::regex("42p 42r");
-    send(KEY_SPACE); stack.pop(2); assert(stack.empty());
-    std::regex("41p 41r");
-    send(KEY_BACKSPACE); stack.pop(2); assert(stack.empty());
-    // enter and delete
-    std::regex("41p 42p 42r");
-    send(KEY_ENTER);
-    std::regex("41p (42p 42r){2}");
-    send(KEY_ENTER); stack.pop(2);
-    std::regex("41p 42p 42r 41r");
-    stack.pop(4); assert(stack.empty());
-    std::regex("42p 41p 41r");
-    send(KEY_DELETE);
-    std::regex("42p (41p 41r){2}");
-    send(KEY_DELETE); stack.pop(2);
-    std::regex("42p 41p 41r 42r");
-    stack.pop(4); assert(stack.empty());
-    // ctrl alt other_key
-    std::regex("(?:41p 42p | 42p 41p) KEYp");
-    press(KEY_CTRL); press(KEY_ALT); press(stack.back());
-    std::regex("(?:41p 42p | 42p 41p) (KEYp) (?:41r|42r)* \1r");
-    release(stack.back());
-    std::regex("(?:41p 42p | 42p 41p) (KEYp) (?:41r|42r)* \1r (?:41r|42r)* KEYp");
-    press(stack.back()); t = stack.back(); stack.pop(3); stack.push_back(t);
-    std::regex("(?:41p 42p | 42p 41p) (?:KEYp (?:41r|42r)*)+ KEYp");
-    press(stack.back());
-    std::regex("(?:41p 42p | 42p 41p) (?:?=(?:KEYp (?:41r|42r)*)+ KEYp) (?:KEYp* (KEYp) KEYp*) \1r");
-    release(stack.back()); stack.remove(p(stack.back()));
-    // ctrl alt done
-    std::regex("(?:41p 42p | 42p 41p) KEYp+ 41r");
-    release(KEY_CTRL);
-    std::regex("(?:41p 42p | 42p 41p) KEYp+ 42r");
-    release(KEY_ALT);
-    std::regex("(?:41p 42p | 42p 41p) KEYp+ 41r 42r");
-    release(KEY_CTRL); release(KEY_ALT); stack.pop(2); stack.pop_front(2);
-    std::regex("(?:41p 42p | 42p 41p) KEYp+ 42r 41r");
-    release(KEY_ALT); release(KEY_CTRL); stack.pop(2); stack.pop_front(2);
-    std::regex("(?:41p 42p | 42p 41p) KEYp+ 42r 42p");
-    press(KEY_ALT); stack.pop(2);
-    std::regex("(?:41p 42p | 42p 41p) KEYp+ 41r 41p");
-    press(KEY_CTRL); stack.pop(2);
+    std::string inputString;
+    while(std:: cin >> inputString) {
+        // space and backspace
+        std::regex("42p 42r");
+        send(KEY_SPACE); stack.pop(2); assert(stack.empty());
+        std::regex("41p 41r");
+        send(KEY_BACKSPACE); stack.pop(2); assert(stack.empty());
+        // enter and delete
+        std::regex("41p 42p 42r");
+        send(KEY_ENTER);
+        std::regex("41p (42p 42r){2}");
+        send(KEY_ENTER); stack.pop(2);
+        std::regex("41p 42p 42r 41r");
+        stack.pop(4); assert(stack.empty());
+        std::regex("42p 41p 41r");
+        send(KEY_DELETE);
+        std::regex("42p (41p 41r){2}");
+        send(KEY_DELETE); stack.pop(2);
+        std::regex("42p 41p 41r 42r");
+        stack.pop(4); assert(stack.empty());
+        // ctrl alt other_key
+        std::regex("(?:41p 42p | 42p 41p) KEYp");
+        press(KEY_CTRL); press(KEY_ALT); press(stack.back());
+        std::regex("(?:41p 42p | 42p 41p) (KEYp) (?:41r|42r)* \1r");
+        release(stack.back());
+        std::regex("(?:41p 42p | 42p 41p) (KEYp) (?:41r|42r)* \1r (?:41r|42r)* KEYp");
+        press(stack.back()); t = stack.back(); stack.pop(3); stack.push_back(t);
+        std::regex("(?:41p 42p | 42p 41p) (?:KEYp (?:41r|42r)*)+ KEYp");
+        press(stack.back());
+        std::regex("(?:41p 42p | 42p 41p) (?:?=(?:KEYp (?:41r|42r)*)+ KEYp) (?:KEYp* (KEYp) KEYp*) \1r");
+        release(stack.back()); stack.remove(p(stack.back()));
+        // ctrl alt done
+        std::regex("(?:41p 42p | 42p 41p) KEYp+ 41r");
+        release(KEY_CTRL);
+        std::regex("(?:41p 42p | 42p 41p) KEYp+ 42r");
+        release(KEY_ALT);
+        std::regex("(?:41p 42p | 42p 41p) KEYp+ 41r 42r");
+        release(KEY_CTRL); release(KEY_ALT); stack.pop(2); stack.pop_front(2);
+        std::regex("(?:41p 42p | 42p 41p) KEYp+ 42r 41r");
+        release(KEY_ALT); release(KEY_CTRL); stack.pop(2); stack.pop_front(2);
+        std::regex("(?:41p 42p | 42p 41p) KEYp+ 42r 42p");
+        press(KEY_ALT); stack.pop(2);
+        std::regex("(?:41p 42p | 42p 41p) KEYp+ 41r 41p");
+        press(KEY_CTRL); stack.pop(2);
+    }
 }

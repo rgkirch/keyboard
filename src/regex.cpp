cbaa870606d40fee948a833b00ad6e0be940aca3
Richie <rgkirch@gmail.com>
2017-02-24 11:19:33 -0500

    using a try for every regex

--- src/regex.cpp
+++ src/regex.cpp
@@ -100,8 +100,8 @@
 #define k46r 0x5c,0x5e
 #define k47r 0x5f
 
-#define KEYp "[0x00-0x2f]"
-#define KEYr "[0x2e-0x5f]"
+#define KEYp [0x00-0x2f]
+#define KEYr [0x2e-0x5f]
 
 enum {KEY_ENTER, KEY_CTRL, KEY_ALT, KEY_DELETE, KEY_BACKSPACE, KEY_SPACE};
 std::string keyStrings[] {"KEY_ENTER", "KEY_CTRL", "KEY_ALT", "KEY_DELETE", "KEY_BACKSPACE", "KEY_SPACE"};
@@ -137,16 +137,21 @@ int main() {
     if(1)
     {
         // space and backspace
-        try
-        {
+        try {
             if (std::regex_match(inputString, std::regex {k42p,k42r} ))
             {
                 send(KEY_SPACE);// stack.pop(2); assert(stack.empty());
             }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
             if (std::regex_match(inputString, std::regex {k41p,k41r} ))
             {
                 send(KEY_BACKSPACE);// stack.pop(2); assert(stack.empty());
             }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
             // enter and delete
             if (std::regex_match(inputString, std::regex {k41p,k42p,k42r} ))
             {
@@ -154,9 +159,7 @@ int main() {
             }
         } catch (std::regex_error e) {
             std::cout << e.what() << std::endl;
-        }
-        try
-        {
+        } try {
             if (std::regex_match(inputString, std::regex {k41p,'(',k42p,k42r,')','\\','{','2','\\','}'} )) {
                 send(KEY_ENTER);// stack.pop(2);
             }
@@ -166,53 +169,96 @@ int main() {
             if (std::regex_match(inputString, std::regex { k41p,k42p,k42r,k41r } )) {
                 //stack.pop(4); assert(stack.empty());
             }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
             if (std::regex_match(inputString, std::regex {k42p,k41p,k41r} )) {
                 send(KEY_DELETE);//
             }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
             if (std::regex_match(inputString, std::regex {k42p,'(',k41p,k41r,')','{','2','}'} )) {
                 send(KEY_DELETE);// stack.pop(2);
             }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
             if (std::regex_match(inputString, std::regex {k42p,k41p,k41r,k42r} )) {
                 //stack.pop(4); assert(stack.empty());
             }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } /*try {
             // ctrl alt other_key
             //if (std::regex_match(inputString, std::regex {'(','?',':',k41p,k42p,'|',k42p,k41p,')',KEYp} )) {
                 //press(KEY_CTRL); press(KEY_ALT);// press(stack.back());
             //}
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
 //            if (std::regex_match(inputString, std::regex {'(','?',':',k41p,k42p,'|', k42p,k41p,')', '(',KEYp,')', ('?',':',k41r'|',k42r,')',* \1r} )) {
 //                //release(stack.back());
 //            }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
 //            if (std::regex_match(inputString, std::regex {'(','?',':',k41p,k42p,'|', k42p,k41p,')', '(',KEYp,')', ('?',':',k41r'|',k42r,')',* \1r ('?',':',k41r'|',k42r,')',* KEYp} )) {
 //                //press(stack.back());// int t = stack.back(); stack.pop(3); stack.push_back(t);
 //            }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
 //            if (std::regex_match(inputString, std::regex {'(','?',':',k41p,k42p,'|', k42p,k41p,')', ('?',':',KEYp ('?',':',k41r'|',k42r,')',*,')','+', KEYp} )) {
 //                //press(stack.back());
 //            }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
 //            if (std::regex_match(inputString, std::regex {'(','?',':',k41p,k42p,'|', k42p,k41p,')', ('?',':','?',=('?',':',KEYp ('?',':',k41r'|',k42r,')',*,')','+', KEYp,')', ('?',':',KEYp* ,'(',KEYp,')', KEYp*,')', \1r} )) {
 //                //release(stack.back());// stack.remove(p(stack.back()));
 //            }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
 //            // ctrl alt done
 //            if (std::regex_match(inputString, std::regex {'(','?',':',k41p,k42p,'|', k42p,k41p,')', KEYp,'+', k41r} )) {
 //                release(KEY_CTRL);
 //            }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
 //            if (std::regex_match(inputString, std::regex {'(','?',':',k41p,k42p,'|', k42p,k41p,')', KEYp,'+', k42r} )) {
 //                release(KEY_ALT);
 //            }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
 //            if (std::regex_match(inputString, std::regex {'(','?',':',k41p,k42p,'|', k42p,k41p,')', KEYp,'+', k41r,k42r} )) {
 //                release(KEY_CTRL); release(KEY_ALT);// stack.pop(2); stack.pop_front(2);
 //            }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
 //            if (std::regex_match(inputString, std::regex {'(','?',':',k41p,k42p,'|', k42p,k41p,')', KEYp,'+', k42r,k41r} )) {
 //                release(KEY_ALT); release(KEY_CTRL);// stack.pop(2); stack.pop_front(2);
 //            }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
 //            if (std::regex_match(inputString, std::regex {'(','?',':',k41p,k42p,'|', k42p,k41p,')', KEYp,'+', k42r,k42p} )) {
 //                press(KEY_ALT);// stack.pop(2);
 //            }
+        } catch (std::regex_error e) {
+            std::cout << e.what() << std::endl;
+        } try {
 //            if (std::regex_match(inputString, std::regex {'(','?',':',k41p,k42p,'|', k42p,k41p,')', KEYp,'+', k41r,k41p} )) {
 //                press(KEY_CTRL);// stack.pop(2);
 //            }
         } catch (std::regex_error e) {
             std::cout << e.what() << std::endl;
         }
+        */
     }
     return 0;
 }

f8058b94f2e6821a9cf614a047292640a220486a
Richard <rgkirch@gmail.com>
2017-03-11 14:58:42 -0500

    added a button to reboot the teensy

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 int numKeys = 48;
+std::vector<int*> keymapLayers;
 
 extern "C" {
     int _getpid(){ return -1;}
@@ -48,7 +49,7 @@ int modifiers[] {
         KEY_TAB, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
         KEY_ESC, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
         KEY_LEFT_SHIFT, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, MODIFIERKEY_GUI, -1, -1, -1, -1, -1, -1, -1, -1,
 };
 int qwerty[] {
         -1, KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T, KEY_Y, KEY_U, KEY_I,     KEY_O,      KEY_P,         KEY_LEFT_BRACE
@@ -60,12 +61,12 @@ int dvorak[] {
         -1, KEY_QUOTE,     KEY_COMMA, KEY_PERIOD, KEY_P, KEY_Y, KEY_F, KEY_G, KEY_C, KEY_R, KEY_L, KEY_SLASH,
         -1, KEY_A,         KEY_O,     KEY_E,	  KEY_U, KEY_I, KEY_D, KEY_H, KEY_T, KEY_N, KEY_S, KEY_MINUS,
         -1, KEY_SEMICOLON, KEY_Q,	  KEY_J,      KEY_K, KEY_X, KEY_B, KEY_M, KEY_W, KEY_V, KEY_Z, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, KEY_BACKSLASH, -1, -1, -1
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
 };
 int numbers[] {
-        KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F11, KEY_F12, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10,
-        -1, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0, -1,
-        -1, KEY_LEFT, KEY_RIGHT, KEY_DOWN, KEY_UP, KEY_LEFT_BRACE, KEY_RIGHT_BRACE, KEY_PAGE_DOWN, KEY_PAGE_UP, KEY_HOME, KEY_END, -1,
+        KEY_F11, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F12,
+        -1, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0, KEY_BACKSLASH,
+        -1, KEY_LEFT, KEY_RIGHT, KEY_DOWN, KEY_UP, KEY_LEFT_BRACE, KEY_RIGHT_BRACE, KEY_PAGE_DOWN, KEY_PAGE_UP, KEY_END, KEY_HOME, -1,
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
 };
 int utils[] {
@@ -76,7 +77,6 @@ int utils[] {
 //       0   1   2   3   4   5 | 6   7   8   9  10  11
 };
 
-std::vector<int*> keymapLayers;
 int get(int layer, int key)
 {
     if(layer < 0) return -1;
@@ -350,9 +350,17 @@ bool thumbs(int action)
     }
     return consumed;
 }
+void reset()
+{
+    _reboot_Teensyduino_();
+}
 bool(*listeners[])(int action) = {shiftEquals, thumbs, layer};
 void push(int action)
 {
+    if (action == k36p)
+    {
+        reset();
+    }
     bool consumed = false;
     for(auto f:listeners)
     {

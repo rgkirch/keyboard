2f7be6fb109120aa74ac01d1cca37556dbff66d8
richie <rgkirch@gmail.com>
2018-03-03 11:53:03 -0500

    reformat

---
+++ test/keyboard.cpp
@@ -0,0 +1,10 @@
+// clang-format off
+#include "keylayout.h"
+#include "keyboard.hpp"
+// clang-format on
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+TEST(hello, one) { cout << "hello there how are you" << endl; }

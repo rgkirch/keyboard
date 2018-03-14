7eb2ffa064c1413d877883d6b0de900ba932d294
richie <rgkirch@gmail.com>
2018-03-14 08:52:56 -0400

    runs but doesn't test

--- test/keyboard.cpp
+++ test/keyboard.cpp
@@ -8,6 +8,7 @@
 
 using ::testing::A;
 using ::testing::AtLeast;
+using ::testing::Eq;
 using ::testing::Invoke;
 using ::testing::Matcher;
 using ::testing::Return;
@@ -20,6 +21,10 @@ using namespace std;
 TEST(hello, one) { cout << "hello there how are you" << endl; }
 TEST(setup, one) {
   EXPECT_CALL(myTestableKeyboardMock, begin()).Times(1);
+  EXPECT_CALL(myTestableMouseMock, begin()).Times(1);
+  EXPECT_CALL(myTestableCoreMock, pinMode(Matcher<uint8_t>(Eq(6)),
+                                          Matcher<uint8_t>(Eq(INPUT_PULLDOWN))))
+      .Times(1);
   setup();
   ASSERT_EQ(2, keymapLayers.size());
 }
